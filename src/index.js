0958381c14a0d849a039246813c22016cfce22cc
polynaris <>
2018-08-13 23:26:49 +0300

    html-webpack-plugin webpack-dev-server

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import User from './user';
+
+const user = new User ('elka', 'novogodnyaja');
+alert(user.sayHi());
+
---
+++ src/user.js
@@ -0,0 +1,9 @@
+export default class User {
+    constructor(firstName, lastName){
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    sayHi(){
+        return `Hello, ${this.firstName} ${this.lastName}`;
+    }
+}
